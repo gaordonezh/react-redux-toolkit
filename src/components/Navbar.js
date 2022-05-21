c87aa1b51615e025a4f6f117eb42767970e3a51e
German <gaordonezh@gmail.com>
2022-05-20 19:25:39 -0500

    NEW FEATURES

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-darl bg-dark">
+      <div className="container">
+        <a className="navbar-brand" href="/home">React Redux App</a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/store/index.js
@@ -0,0 +1,6 @@
+import { configureStore } from "@reduxjs/toolkit";
+import users from "./slices/users";
+
+export default configureStore({
+  reducer: { users },
+});
